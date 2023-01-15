b3164feb1af3ff220cb8822b4f4f18a968860cfa
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-01-15 10:19:16 +0530

    added static URLs
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- auditions/settings.py
+++ auditions/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
