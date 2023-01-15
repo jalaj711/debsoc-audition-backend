9df239fb8ddfe9b7419601352e17fb1c947f5b94
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-01-15 08:44:50 +0530

    add model Candidate
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Candidate(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    number = models.CharField(max_length=100, blank=False)
+    rollno = models.CharField(max_length=100, blank=False)
+    hallno = models.CharField(max_length=100, blank=False)
+    section = models.CharField(max_length=100, blank=False)
+    club_preference = models.CharField(max_length=300, blank=False)
+    roles_preference = models.CharField(max_length=300, blank=False)
+    answer_1_text = models.TextField(max_length=1000, blank=False)
+    answer_2_text = models.TextField(max_length=1000, blank=False)
+    answer_3_text = models.TextField(max_length=1000, blank=False)
