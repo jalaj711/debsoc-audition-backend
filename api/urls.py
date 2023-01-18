bb85fcf63f6e29dd90e5dc56811657ce0c0ba43f
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-01-18 07:59:35 +0530

    added csv export endpoints
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- api/urls.py
+++ api/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
          csrf_exempt(views.register.as_view()), name='audition_registration'),
     path('check-email/',
          csrf_exempt(views.register_check_email.as_view()), name='audition_check_email'),
+    path('export/',
+         csrf_exempt(views.export_registrations), name='export'),
+    path('export-all/',
+         csrf_exempt(views.export_registrations_all), name='export_all'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
+from django.http import HttpResponse
 from .models import Candidate
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework import generics
 from rest_framework.response import Response
 from django.db.utils import IntegrityError
+import csv
 
 # Create your views here.
 
@@ -60,3 +62,60 @@ class register_check_email(generics.GenericAPIView):
             pass
 
         return Response({"registered": registered})
+
+
+def export_registrations(request):
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = 'attachment; filename="auditions.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['name', 'email', 'number', 'rollno', 'hallno'])
+    for cdt in Candidate.objects.all():
+        writer.writerow(
+            [
+                cdt.name,
+                cdt.email,
+                cdt.number,
+                cdt.rollno,
+                cdt.hallno,
+            ]
+        )
+    return response
+
+
+def export_registrations_all(request):
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = 'attachment; filename="auditions_all.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['name', 'email', 'number', 'rollno', 'hallno', 'club_preference',
+                     'roles_preference',
+                     'answer_1_text',
+                     'answer_2_text',
+                     'answer_3_text',
+                     'answer_4_text',
+                     'answer_5_text',
+                     'answer_6_text',
+                     'answer_7_text',
+                     'answer_8_text',
+                     'answer_9_text'])
+    for cdt in Candidate.objects.all():
+        writer.writerow(
+            [
+                cdt.name,
+                cdt.email,
+                cdt.number,
+                cdt.rollno,
+                cdt.hallno,
+                cdt.club_preference,
+                cdt.roles_preference,
+                cdt.answer_1_text,
+                cdt.answer_2_text,
+                cdt.answer_3_text,
+                cdt.answer_4_text,
+                cdt.answer_5_text,
+                cdt.answer_6_text,
+                cdt.answer_7_text,
+                cdt.answer_8_text,
+                cdt.answer_9_text
+            ]
+        )
+    return response
